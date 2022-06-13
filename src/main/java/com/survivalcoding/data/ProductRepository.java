48b3037af661a6a0cb187b84a24e679f8df23349
woococo <j2922y@gmail.com>
2022-06-13 11:24:17 +0900

    ProductRepository

--- src/main/java/com/survivalcoding/data/ProductRepository.java
+++ src/main/java/com/survivalcoding/data/ProductRepository.java
@@ -1,33 +1,42 @@
 package com.survivalcoding.data;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 import com.survivalcoding.domain.model.Product;
 
 // 다형성
 public class ProductRepository {
-    public static void main(String[] args) {
-        // 삽입, 삭제가 빈번할 때 항상 동일 성능, but ArrayList보다 검색이 느려
-        // 메모리 조금 더 많이 먹어
-        List<Product> products = new LinkedList<>();
+    private List<Product> products = new ArrayList<>();
 
-        // 크기가 커질수록 삽입, 삭제가 느려짐, 내부적으로 배열이라 성능이 일단 빨라
-        List<Product> products2 = new ArrayList<>();
+    public ProductRepository() {
+        Product phone = new Product("P1234", "iPhone 6s", 800000);
+        phone.setDescription("4.7-inch, 1334x750 Retina HD display");
+        phone.setCategory("Smart Phone");
+        phone.setManufacturer("Apple");
+        phone.setUnitsInStock(1000);
+        phone.setCondition("New");
 
-        Product[] product3 = new Product[3];
+        Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+        notebook.setDescription("!4.7-inch, 1334x750 Retina HD display");
+        notebook.setCategory("!Smart Phone");
+        notebook.setManufacturer("!Apple");
+        notebook.setUnitsInStock(1000);
+        notebook.setCondition("Refubished");
 
-        sort(Arrays.asList(product3));
+        Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+        tablet.setDescription("?4.7-inch, 1334x750 Retina HD display");
+        tablet.setCategory("?Smart Phone");
+        tablet.setManufacturer("?Apple");
+        tablet.setUnitsInStock(1000);
+        tablet.setCondition("Old");
 
-        sort(products);
-        Stack stack = (Stack) sort(products2);
+        products.add(phone);
+        products.add(notebook);
+        products.add(tablet);
     }
 
-    public static List<Product> sort(List<Product> products) {
-        // 정렬.....
-        return new LinkedList<>();
+    public List<Product> getAllProducts() {
+        return products;
     }
+
 }
-
