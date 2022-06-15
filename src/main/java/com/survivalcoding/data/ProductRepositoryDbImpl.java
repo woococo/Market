af1ce52b52e3c5e4bf4cfe87c099c7e34fddf5f6
woococo <j2922y@gmail.com>
2022-06-15 14:53:52 +0900

    상품 등록 DB 연결 성공

--- src/main/java/com/survivalcoding/data/ProductRepositoryDbImpl.java
+++ src/main/java/com/survivalcoding/data/ProductRepositoryDbImpl.java
@@ -21,12 +21,16 @@ public class ProductRepositoryDbImpl implements ProductRepository {
 	@Override
 	public Product getProductById(String id) {
 		// TODO Auto-generated method stub
-		return null;
+		return getAllProducts().stream()
+                .filter((product) -> product.getId().equals(id)) // 조건
+                .findFirst() // 첫번째
+                .get(); // 얻어
 	}
 
 	@Override
 	public void addProduct(Product product) {
 		// TODO Auto-generated method stub
+		dao.insert(product);
 
 	}
 
