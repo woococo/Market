e042395288ccb7e429367d4b264c603d3e794827
woococo <j2922y@gmail.com>
2022-06-15 13:40:58 +0900

    DB 연결 성공!

--- src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
+++ src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
@@ -1,40 +1,74 @@
 package com.survivalcoding.data.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import com.survivalcoding.domain.model.Product;
 
-public class MySqlProductDaoImpl implements ProductDao{
-	
-	public MySqlProductDaoImpl() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("jdbc 드라이버 로드 실패");
-		}
-	}
+public class MySqlProductDaoImpl implements ProductDao {
 
-	@Override
-	public List<Product> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public MySqlProductDaoImpl() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("jdbc 드라이버 로드 실패");
+        }
+    }
 
-	@Override
-	public void insert(Product product) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<Product> getAll() {
+        List<Product> results = new ArrayList<>();
 
-	@Override
-	public void update(Product product) {
-		// TODO Auto-generated method stub
-		
-	}
+        String sql = "SELECT * FROM product";
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.23.98:33060/kopoctc", "root", "qwer");
+                Statement stmt = conn.createStatement();) {
+            try (ResultSet rs = stmt.executeQuery(sql)) {
+                while (rs.next()) {
+                    String id = rs.getString("p_id");
+                    String name = rs.getString("p_name");
+                    int unitPrice = rs.getInt("p_unitPrice");
+                    String description = rs.getString("p_description");
+                    String category = rs.getString("p_category");
+                    String manufacturer = rs.getString("p_manufacturer");
+                    long unitsInStock = rs.getLong("p_unitsInStock");
+                    String condition = rs.getString("p_condition");
 
-	@Override
-	public void delete(Product product) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+                    Product product = new Product(id, name, unitPrice);
+                    product.setDescription(description);
+                    product.setCategory(category);
+                    product.setManufacturer(manufacturer);
+                    product.setUnitsInStock(unitsInStock);
+                    product.setCondition(condition);
+
+                    results.add(product);
+                }
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("db 연결 실패");
+        }
+        return results;
+    }
+
+    @Override
+    public void insert(Product product) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(Product product) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete(Product product) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
