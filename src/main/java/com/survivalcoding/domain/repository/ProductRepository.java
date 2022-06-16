c8adff3949a86ffb0a0db87ae976c9daddc92ee0
woococo <j2922y@gmail.com>
2022-06-16 10:14:51 +0900

    MVC Model 2, Servlet 경험

--- src/main/java/com/survivalcoding/domain/repository/ProductRepository.java
+++ src/main/java/com/survivalcoding/domain/repository/ProductRepository.java
@@ -1,22 +1,20 @@
 package com.survivalcoding.domain.repository;
 
 import java.util.List;
-
 import com.survivalcoding.data.ProductRepositoryDbImpl;
-import com.survivalcoding.data.ProductRepositoryImpl;
 import com.survivalcoding.data.dao.MySqlProductDaoImpl;
 import com.survivalcoding.domain.model.Product;
 
 public interface ProductRepository {
 
-	public static ProductRepository getInstance() {
-		return new ProductRepositoryDbImpl(new MySqlProductDaoImpl());
+    public static ProductRepository getInstance() {
+//        return ProductRepositoryImpl.getInstance();
+         return new ProductRepositoryDbImpl(new MySqlProductDaoImpl());
+    }
 
-	}
-
-	public List<Product> getAllProducts();
-
-	public Product getProductById(String id);
-
-	public void addProduct(Product product);
+    public List<Product> getAllProducts();
+    
+    public Product getProductById(String id);
+    
+    public void addProduct(Product product);
 }
